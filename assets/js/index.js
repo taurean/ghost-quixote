7e52393827b0537cde80c46be4bb3c3172f74264
Taurean Bryant <taureanbryant@me.com>
2016-08-02 15:39:38 -0700

    fix for the availability date

--- assets/js/index.js
+++ assets/js/index.js
@@ -76,3 +76,27 @@
 
    toggleButton.addEventListener('click', handleToggle);
  };
+
+
+ // Availability link
+
+ (function loadData(){
+     var anchor = document.getElementById("availabilityLink");
+     var xhr = new XMLHttpRequest();
+     xhr.open("GET", "https://my.cushionapp.com/api/v1/users/7c6d447d-4904-402b-b154-08077b3220fb/availability");
+     xhr.addEventListener('load', function() {
+         var label = "currently unavailable";
+         if (xhr.status >= 200 && xhr.status < 300) {
+             try {
+                 var parsed = JSON.parse(xhr.response);
+                 var weekInMs = 7*24*60*60*1000;
+                 var timeToAvailable = new Date(parsed.availability.available_on) - new Date();
+
+                 if (timeToAvailable < weekInMs) label = "available";
+                 else if (timeToAvailable < weekInMs*4) label = "available soon";
+             } catch(e) { console.error(e); }
+         }
+         anchor.innerText = label;
+     });
+     xhr.send();
+ })();
